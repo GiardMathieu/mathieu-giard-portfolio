9a197a157aa59205100369baa96d2863f606e8fb
MathieuGiardEts <mathieu.giard.1@ens.etsmtl.ca>
2024-07-30 12:30:09 -0400

    Home page changed

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -13,7 +13,8 @@ export default function Home() {
 		<div className="hero-content text-center">
 			<div className="max-w-md">
 				<h1 className="text-5xl font-bold">Mathieu Giard</h1>
-				<p className="py-6">Développeur Junior</p>
+				<h2 className="py-3">Développeur Junior</h2>
+				<p className="pb-4">Bonjour, je m'appelles Mathieu Giard et je suis un développeur qui a reçu le diplôme d'études collégiales (DEC) dans le programme de développement d'applications. J'ai conçu ce portfolio pour me permettre de mettre en valeur mes capacités et de montrer les projets de programmation dont je suis fier. </p>
 				<p><Link  href="portfolio" className="btn btn-secondary">Aller au Portfolio</Link></p>
 				<p>
 					<Link className="btn btn-info mt-5" href="/Mathieu_GiardCV.pdf">Visionner le Curriculum Vitae (CV)</Link>
